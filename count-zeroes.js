b3f3fdda9b82e23ce64e8a211fd483d66286f0c6
Virginia Wu <virginiawu11@gmail.com>
2021-12-31 22:33:31 -0500

    complete countZeroes & sortedFrequency

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,17 @@
-function countZeroes() {
-  
+function countZeroes(arr, leftIndex = 0, rightIndex = arr.length - 1) {
+  if (arr[arr.length - 1] === 1) return 0;
+  if (arr[0] === 0) return arr.length;
+
+  let midIndex = Math.floor((leftIndex + rightIndex) / 2);
+  if (arr[midIndex] > arr[midIndex + 1]) {
+    return arr.length - midIndex - 1;
+  }
+  if (arr[midIndex] === 1) {
+    return countZeroes(arr, (leftIndex = midIndex + 1), rightIndex);
+  }
+  if (arr[midIndex] === 0) {
+    return countZeroes(arr, leftIndex, (rightIndex = midIndex - 1));
+  }
 }
 
-module.exports = countZeroes
+module.exports = countZeroes;
--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,74 @@
-function sortedFrequency() {
-
+function sortedFrequency(arr, target) {
+  let firstIndex, lastIndex;
+  if (arr[0] === target) {
+    firstIndex = 0;
+  } else {
+    firstIndex = findFirstIndex(arr, target);
+  }
+  if (arr[arr.length - 1] === target) {
+    lastIndex = arr.length - 1;
+  } else {
+    lastIndex = findLastIndex(arr, target);
+  }
+  if (firstIndex === -1 || lastIndex === -1) {
+    return -1;
+  }
+  return 1 + lastIndex - firstIndex;
 }
 
-module.exports = sortedFrequency
+function findFirstIndex(
+  arr,
+  target,
+  leftIndex = 0,
+  rightIndex = arr.length - 1
+) {
+  let midIndex = Math.floor((leftIndex + rightIndex) / 2);
+  if (leftIndex > rightIndex) {
+    return -1;
+  }
+  if (arr[midIndex] === target) {
+    if (arr[midIndex - 1] < target) {
+      return midIndex;
+    } else {
+      return findFirstIndex(
+        arr,
+        target,
+        leftIndex,
+        (rightIndex = midIndex - 1)
+      );
+    }
+  }
+  if (arr[midIndex] > target) {
+    return findFirstIndex(arr, target, leftIndex, (rightIndex = midIndex - 1));
+  }
+  if (arr[midIndex] < target) {
+    return findFirstIndex(arr, target, (leftIndex = midIndex + 1), rightIndex);
+  }
+}
+function findLastIndex(
+  arr,
+  target,
+  leftIndex = 0,
+  rightIndex = arr.length - 1
+) {
+  let midIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+  if (leftIndex > rightIndex) {
+    return -1;
+  }
+  if (arr[midIndex] === target) {
+    if (arr[midIndex + 1] > target) {
+      return midIndex;
+    } else {
+      return findLastIndex(arr, target, (leftIndex = midIndex + 1), rightIndex);
+    }
+  }
+  if (arr[midIndex] > target) {
+    return findLastIndex(arr, target, leftIndex, (rightIndex = midIndex - 1));
+  }
+  if (arr[midIndex] < target) {
+    return findLastIndex(arr, target, (leftIndex = midIndex + 1), rightIndex);
+  }
+}
+
+module.exports = sortedFrequency;
