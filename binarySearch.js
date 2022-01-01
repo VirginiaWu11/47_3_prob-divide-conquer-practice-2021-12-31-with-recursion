8edca311e03bb0ef20adca00c04293b7e6e329bf
Virginia Wu <virginiawu11@gmail.com>
2021-12-31 21:55:55 -0500

    comment out incorrect version

--- binarySearch.js
+++ binarySearch.js
@@ -14,38 +14,39 @@
 //   return -1;
 // }
 
-// function binarySearch(arr, target, leftIndex = 0, rightIndex = arr.length - 1) {
-//   if (leftIndex > rightIndex) return -1;
-//   let midIndex = Math.floor((leftIndex + rightIndex) / 2);
-//   if (arr[midIndex] < target) {
-//     return binarySearch(arr, target, (leftIndex = midIndex + 1), rightIndex);
-//   }
-//   if (arr[midIndex] > target) {
-//     return binarySearch(arr, target, leftIndex, (rightIndex = midIndex - 1));
-//   }
-//   if (arr[midIndex] === target) {
-//     return midIndex;
-//   }
-// }
-
-function binarySearch(arr, target) {
-  let leftIndex = 0;
-  let rightIndex = arr.length - 1;
+function binarySearch(arr, target, leftIndex = 0, rightIndex = arr.length - 1) {
+  if (leftIndex > rightIndex) return -1;
   let midIndex = Math.floor((leftIndex + rightIndex) / 2);
-  console.log({ leftIndex, rightIndex, midIndex, arr });
-  if (arr.length === 0) {
-    return -1;
+  if (arr[midIndex] < target) {
+    return binarySearch(arr, target, (leftIndex = midIndex + 1), rightIndex);
+  }
+  if (arr[midIndex] > target) {
+    return binarySearch(arr, target, leftIndex, (rightIndex = midIndex - 1));
   }
   if (arr[midIndex] === target) {
     return midIndex;
   }
-  if (arr[midIndex] < target) {
-    return binarySearch(arr.slice(midIndex + 1), target);
-  }
-  if (arr[midIndex] > target) {
-    return binarySearch(arr.slice(0, midIndex), target);
-  }
 }
+
+// incorrect:
+// function binarySearch(arr, target) {
+//   let leftIndex = 0;
+//   let rightIndex = arr.length - 1;
+//   let midIndex = Math.floor((leftIndex + rightIndex) / 2);
+//   console.log({ leftIndex, rightIndex, midIndex, arr });
+//   if (arr.length === 0) {
+//     return -1;
+//   }
+//   if (arr[midIndex] === target) {
+//     return midIndex;
+//   }
+//   if (arr[midIndex] < target) {
+//     return binarySearch(arr.slice(midIndex + 1), target);
+//   }
+//   if (arr[midIndex] > target) {
+//     return binarySearch(arr.slice(0, midIndex), target);
+//   }
+// }
 
 console.log(binarySearch([0, 1, 2, 5, 6, 7, 8, 9, 10], 10)); //8
 // console.log(binarySearch([0, 1, 2, 5, 6, 7, 8, 9, 10], 1)); //1
